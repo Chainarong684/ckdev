dc0c1f83c345ed953b52d5293697709f8a907902
CK <9k.chainarong@gmail.com>
2021-02-23 23:59:29 +0700

    commit by vscode

--- routes/blog.js
+++ routes/blog.js
@@ -1,17 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const {
+  body,
+  validationResult,
+  check
+} = require('express-validator');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('blog');
 });
 
-router.get('/addBlog', function(req, res, next) {
+router.get('/addBlog', function (req, res, next) {
   res.render('addBlog');
 });
 
-router.post('/addBlog', function(req, res, next) {
-  
+router.post('/addBlog', [
+  check('articleName', 'You must input Article Name').notEmpty(),
+  check('article', 'Please input some text').notEmpty()
+], function (req, res, next) {
+
+  const results = validationResult(req);
+
+  var resultError = results.errors;
+
+  // console.log(resultError);
+
+  if (!results.isEmpty()) {
+    console.log(results);
+
+    res.render('addBlog', {resultError});
+  }
+
+  console.log(req.body.articleName);
+  console.log(req.body.article);
 });
 
 
